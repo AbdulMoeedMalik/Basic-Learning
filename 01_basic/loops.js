922f10125ee94fe7e2f9536b42a59045253f3b73
Malik Moeed <amoeed923@gmail.com>
2025-03-11 10:23:49 +0000

    Loops

--- 01_basic/loops.js
+++ 01_basic/loops.js
@@ -1,11 +1,18 @@
-// // for Loop
+// // for Loop Used when you know how many times to run the loop.
+
+
 //  let a = 5 ;
 // for (let i = 5; i < 51; i = i + 5) {
 //     console.log( i);
     
 // }
 
-// // for in loop
+// // for in Used to loop through keys of an object.
+
+javascript
+Copy
+Edit
+
 
 // let obj ={
 //     name: "Moeed",
@@ -19,7 +26,7 @@
               
 // }
 
-// While Loop
+// While Loop  Used when you don’t know how many times the loop will run.
 
 // let i = 0 ;
 
@@ -28,8 +35,8 @@
 //      i++;
      
 // }
-
-// do {
+ 
+// do {                 Runs at least once, even if the condition is false at the start.
 //     console.log(i);
 //      i++;
 // } while (i<6);
